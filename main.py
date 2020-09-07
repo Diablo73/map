5f713926b4e8982b0a9c640a8f3d582b15e8afe7
Diablo73 <66016428+Diablo73@users.noreply.github.com>
2020-09-07 21:35:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ city = list(data["CITY"])
 
 fgc = folium.FeatureGroup(name="Cricket Stadiums")
 for lt, ln, n, c in zip(lat, lon, name, city):
-    fgc.add_child(folium.Marker(location=[lt, ln], radius=1, popup=n + ", " + c, icon=folium.Icon(color="lightgreen")))
+	fgc.add_child(folium.Marker(location=[lt, ln], radius=1, popup=n + ", " + c, icon=folium.Icon(color="lightgreen")))
 print("Cricket Stadiums FILE DONE.......")
 
 fgp = folium.FeatureGroup(name="Population")
@@ -31,12 +31,12 @@ iata = list(data["IATA"])
 fgam = folium.FeatureGroup(name="Medium Airports", show=False)
 fgal = folium.FeatureGroup(name="Large Airports", show=False)
 for lt, ln, n, t, cou, ci, i in zip(lat, lon, name, typ, country, city, iata):
-    if t == "MED":
-        fgam.add_child(folium.Marker(location=[lt, ln], radius=0.20, popup=n + "(" + i + "), " + ci + ", " + str(cou), icon=folium.Icon(color="lightblue")))
-    elif t == "LAR":
-        fgal.add_child(folium.Marker(location=[lt, ln], radius=0.75, popup=n + "(" + i + "), " + ci + ", " + str(cou), icon=folium.Icon(color="lightred")))
-    else:
-        print("Check your Airport")
+	if t == "MED":
+		fgam.add_child(folium.Marker(location=[lt, ln], radius=0.20, popup=n + "(" + i + "), " + ci + ", " + str(cou), icon=folium.Icon(color="lightblue")))
+	elif t == "LAR":
+		fgal.add_child(folium.Marker(location=[lt, ln], radius=0.75, popup=n + "(" + i + "), " + ci + ", " + str(cou), icon=folium.Icon(color="lightred")))
+	else:
+		print("Check your Airport")
 print("Airports FILE DONE.......")
 
 print()
